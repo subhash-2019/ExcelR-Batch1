0c425b5a4f34c8621c438457ed57b6f58b096361
subhash-2019 <gudipatisubhash20@gmail.com>
2025-01-21 22:16:45 +0530

    If else

---
+++ Demo2.java
@@ -0,0 +1,10 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        int x = 2;
+        if (x > 0) {
+            System.out.println("x is a positive number");
+        } else {
+            System.out.println("x is a negative number");
+        }
+    }
+}
