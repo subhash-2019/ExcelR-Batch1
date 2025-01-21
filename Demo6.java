f37c7c683e4a9c233fbd07ce765ce99d12470eb3
subhash-2019 <gudipatisubhash20@gmail.com>
2025-01-21 22:18:33 +0530

    While loop 2nd

---
+++ Demo6.java
@@ -0,0 +1,12 @@
+public class Demo6 {
+
+    public static void main(String[] args) {
+        String username = "Spoorthi";
+        String pwd = "spoorthi_01";
+        while (username == "Spoorthi" && pwd == "spoorthi_01") {
+            System.out.println("Hello User,Welcome to Mallareddy University");
+            break;
+        }
+    }
+
+}
