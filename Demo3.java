15628b63882bee0a2e328ed75be49e85de512626
subhash-2019 <gudipatisubhash20@gmail.com>
2025-01-21 22:17:25 +0530

    If elseif

---
+++ Demo3.java
@@ -0,0 +1,17 @@
+public class Demo3 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+        int c = 40;
+        if (a > b && a > c) {
+            System.out.println("a is greater than b and c");
+        } else if (b > a && b > c) {
+            System.out.println("b is greater than a and c");
+
+        } else {
+            System.out.println("c is greater than a and b");
+        }
+
+    }
+
+}
