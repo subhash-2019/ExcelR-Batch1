0b4f15348fc2e8f9dfd99e13cf4e04189b02f1c2
subhash-2019 <gudipatisubhash20@gmail.com>
2025-01-21 22:18:11 +0530

    While loop

---
+++ Demo5.java
@@ -0,0 +1,11 @@
+public class Demo5 {
+    public static void main(String[] args) {
+        String username = "user";
+        String pwd = "user_01";
+        while (username == "user" && pwd == "user_01") {
+            System.out.println("Hello User");
+            break;
+        }
+    }
+
+}
