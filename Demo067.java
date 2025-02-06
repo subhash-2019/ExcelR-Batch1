6837dfe20d62500af99d19eaec7d7e90be90ce2b
subhash-2019 <gudipatisubhash20@gmail.com>
2025-02-06 10:56:41 +0530

    Add files via upload

---
+++ Demo067.java
@@ -0,0 +1,15 @@
+public class Demo067 {
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 7};
+        for (int i = 0; i < arr.length; i++) {
+        if(i % 2 == 1) {
+            System.out.println("Odd");
+        } else {
+            System.out.println("Even");
+         
+    }  
+    }
+    
+}
+    
+}
